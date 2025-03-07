ad110e31d1e4375d568a3710c59686468e6b7b11
n_i_p_u_n <112545976+nipunsw1999@users.noreply.github.com>
2025-03-07 18:19:52 +0530

    youtube feature added to test

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 import streamlit as st 
+from pytube import YouTube
+
+st.title("Video Downloader")
+
+url = st.text_input("Enter the video URL")
+
+if url:
+    yt = YouTube(url)
+    st.write("Title:", yt.title)
+    st.image(yt.thumbnail_url)
