374f5c8fe792d576372c238be79e3ef86d493826
n_i_p_u_n <112545976+nipunsw1999@users.noreply.github.com>
2025-03-07 20:05:50 +0530

    app updated

--- app.py
+++ app.py
@@ -7,22 +7,21 @@ st.title("Video Downloader")
 if 'video_info' not in st.session_state:
     st.session_state['video_info'] = None
     
-video_url = st.text_input("Enter video URL")
 
-if video_url:
-    st.write("Fetching video details...")
-    with yt_dlp.YoutubeDL() as ydl:
-        video_info = ydl.extract_info(video_url, download=False)
-        col1, col2, col3 = st.columns([3,1,3])
-        with col1:
-            st.write("Title")
-            st.write("Uploader")
-            st.write("Duration")
-        with col2:
-            st.write(" - ")
-            st.write(" - ")
-            st.write(" - ")
-        with col3:
-            st.write(video_info['title'])
-            st.write(video_info['uploader'])
-            st.write(video_info['duration'])
+
+
+tab1,tab2 = st.tabs(["Single Video download", "Playlist download"])
+with tab1:
+    video_url = st.text_input("Enter video URL")
+    if video_url:
+        select_resolution = st.selectbox(["1080","720","480","360"])
+        if select_resolution:
+            ydl_opts = {
+                'format':'(bestvideo[height-{select_resolution}]+bestaudio)/',
+                'merge_output_format': 'mp4',
+            }
+        with yt_dlp.YoutubeDL() as ydl:
+            video_info = ydl.extract_info(video_url, download=False)
+            ydl.download(video_url)
+with tab2:
+    st.write("Playlist download")
