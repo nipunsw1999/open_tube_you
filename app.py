5579a37a63a9090ed52de47e31dd46330f70bfbe
n_i_p_u_n <112545976+nipunsw1999@users.noreply.github.com>
2025-03-07 22:31:20 +0530

    app updated

--- app.py
+++ app.py
@@ -4,22 +4,52 @@ from functions import duration_show
 
 st.title("Video Downloader")
 
+if "stat" not in st.session_state:
+    st.session_state.stat = False
+
+
 tab1, tab2 = st.tabs(["Single Video download", "Playlist download"])
 
 with tab1:
     video_url = st.text_input("Enter video URL")
-    if video_url:
-        yt = YouTube(video_url)
-        st.image(yt.thumbnail_url)
-        st.write(yt.title)
-        st.write(duration_show(yt.length))
-        select_resolution = st.selectbox("Select resolution", ["1080", "720", "480", "360"])
-        download_btn = st.button("Download video")
-        if select_resolution:
-            pass
+    search_btn = st.button("Search video")
+    if search_btn:
+        st.session_state.stat = True
+    if video_url == "": st.session_state.stat = False
+    if st.session_state.stat:
+            yt = YouTube(video_url)
+            st.image(yt.thumbnail_url)
+            st.write(yt.title)
+            st.write(duration_show(yt.length))
             
-        if download_btn:
-                st.success("Video Download Success")
-
+            # Build Resolutions
+            resolutions = [stream.resolution for stream in yt.streams.filter(adaptive=True)]
+            
+            # Filter and arrange
+            cleaned_resolutions = []
+            seen_none = False
+            for res in resolutions:
+                if res == "None":
+                    if not seen_none:
+                        cleaned_resolutions.append(res)
+                        seen_none = True
+                else:
+                    if res not in cleaned_resolutions:
+                        cleaned_resolutions.append(res)
+            sorted_resolutions = sorted(cleaned_resolutions[1:], key=lambda x: int(x.replace('p', '')) if x else 0)
+            if "None" in resolutions:
+                sorted_resolutions.insert(0, "None")
+            select_resolution = st.selectbox("Select Video Quality", sorted_resolutions)
+            download_btn = st.button("Download")
+            if download_btn and select_resolution != "None":
+                yt.streams.filter(adaptive=True, resolution=select_resolution).first().download(output_path="downloads")
+                st.success("Downloaded successfully")
+            elif download_btn and select_resolution == "None":
+                st.error("Select a resolution")
+            
+        # except Exception as e:
+        #     st.error("Invalid video URL")
+        #     st.session_state.stat = False
+    
 with tab2:
     st.write("Playlist download")
