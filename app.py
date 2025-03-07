fc8bf5b8b1d3311d422656fe587dc11e8e856b53
n_i_p_u_n <112545976+nipunsw1999@users.noreply.github.com>
2025-03-07 20:19:24 +0530

    app updated

--- app.py
+++ app.py
@@ -3,25 +3,26 @@ import yt_dlp
 
 st.title("Video Downloader")
 
+tab1, tab2 = st.tabs(["Single Video download", "Playlist download"])
 
-if 'video_info' not in st.session_state:
-    st.session_state['video_info'] = None
-    
-
-
-
-tab1,tab2 = st.tabs(["Single Video download", "Playlist download"])
 with tab1:
     video_url = st.text_input("Enter video URL")
     if video_url:
-        select_resolution = st.selectbox(["1080","720","480","360"])
+        select_resolution = st.selectbox("Select resolution", ["1080", "720", "480", "360"])
+        download_btn = st.button("Download video")
         if select_resolution:
+            # Use f-string for dynamic resolution
             ydl_opts = {
-                'format':'(bestvideo[height-{select_resolution}]+bestaudio)/',
+                'format': f'bestvideo[height={select_resolution}]+bestaudio/best[height={select_resolution}]',
                 'merge_output_format': 'mp4',
             }
-        with yt_dlp.YoutubeDL() as ydl:
-            video_info = ydl.extract_info(video_url, download=False)
-            ydl.download(video_url)
+            
+        if download_btn:
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                video_info = ydl.extract_info(video_url, download=False)
+                st.write(f"Video info: {video_info['title']}")
+                ydl.download([video_url])
+                st.success("Video Download Success")
+
 with tab2:
     st.write("Playlist download")
