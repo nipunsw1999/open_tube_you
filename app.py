5695a79290f9a649b33f8895d4fd4a3239d0e353
n_i_p_u_n <112545976+nipunsw1999@users.noreply.github.com>
2025-03-07 20:34:12 +0530

    app updated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import yt_dlp
+from pytubefix import YouTube
 
 st.title("Video Downloader")
 
@@ -11,17 +11,9 @@ with tab1:
         select_resolution = st.selectbox("Select resolution", ["1080", "720", "480", "360"])
         download_btn = st.button("Download video")
         if select_resolution:
-            # Use f-string for dynamic resolution
-            ydl_opts = {
-                'format': f'bestvideo[height={select_resolution}]+bestaudio/best[height={select_resolution}]',
-                'merge_output_format': 'mp4',
-            }
+            pass
             
         if download_btn:
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                video_info = ydl.extract_info(video_url, download=False)
-                st.write(f"Video info: {video_info['title']}")
-                ydl.download([video_url])
                 st.success("Video Download Success")
 
 with tab2:
