1d07fa30a93175e09c26f2da65bdf244c1cf18f7
n_i_p_u_n <112545976+nipunsw1999@users.noreply.github.com>
2025-03-07 20:39:13 +0530

    bug fixed

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ tab1, tab2 = st.tabs(["Single Video download", "Playlist download"])
 with tab1:
     video_url = st.text_input("Enter video URL")
     if video_url:
+        yt = YouTube(video_url)
+        st.write(yt.title)
+        st.image(yt.thumbnail_url)
         select_resolution = st.selectbox("Select resolution", ["1080", "720", "480", "360"])
         download_btn = st.button("Download video")
         if select_resolution:
