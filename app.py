61cda3ba4321ef883832898afd780b7f89046bff
n_i_p_u_n <112545976+nipunsw1999@users.noreply.github.com>
2025-03-07 18:28:55 +0530

    youtube feature updated

--- app.py
+++ app.py
@@ -4,8 +4,15 @@ from pytube import YouTube
 st.title("Video Downloader")
 
 url = st.text_input("Enter the video URL")
+check = st.button("Check")
 
-if url:
-    yt = YouTube(url)
-    st.write("Title:", yt.title)
-    st.image(yt.thumbnail_url)
+
+if check and url !="":
+    try:
+        with st.spinner("Wait for it...", show_time=True):
+            yt = YouTube(url)
+        st.success("Done!")
+        st.write("Title:", yt.title)
+        st.image(yt.thumbnail_url)
+    except Exception as e:
+        st.write(e)
