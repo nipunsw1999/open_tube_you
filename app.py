bc21b8ae3dd3c17ef8e5c2784670e6daf1bfc42a
n_i_p_u_n <112545976+nipunsw1999@users.noreply.github.com>
2025-03-07 19:42:42 +0530

    app updated

--- app.py
+++ app.py
@@ -3,10 +3,26 @@ import yt_dlp
 
 st.title("Video Downloader")
 
+
+if 'video_info' not in st.session_state:
+    st.session_state['video_info'] = None
+    
 video_url = st.text_input("Enter video URL")
 
 if video_url:
     st.write("Fetching video details...")
     with yt_dlp.YoutubeDL() as ydl:
         video_info = ydl.extract_info(video_url, download=False)
-        st.write(video_info)
+        col1, col2, col3 = st.columns([3,1,3])
+        with col1:
+            st.write("Title")
+            st.write("Uploader")
+            st.write("Duration")
+        with col2:
+            st.write(" - ")
+            st.write(" - ")
+            st.write(" - ")
+        with col3:
+            st.write(video_info['title'])
+            st.write(video_info['uploader'])
+            st.write(video_info['duration'])
